6eccb2c6a3a35b69b7a646b244c05820fce0fa0e
JoP <jop.mezzana@gmail.com>
2017-08-15 17:43:55 +0200

    Deleted old project files

--- src/utils/stores/store.module.js
+++
@@ -1,8 +0,0 @@
-import Store from 'utils/stores/store'
-import ClientStore from 'utils/stores/clientStore'
-
-export default angular
-  .module('myVet.store', [])
-  .factory(Store.name, Store.service)
-  .factory(ClientStore.name, ClientStore.service)
-  .name
