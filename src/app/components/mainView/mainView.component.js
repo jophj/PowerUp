299ccc1bb60b419fe801be75dd03d3ba94fb743d
JoP <jop.mezzana@gmail.com>
2017-08-16 23:29:05 +0200

    Configuration buids table

--- src/app/components/mainView/mainView.component.js
+++ src/app/components/mainView/mainView.component.js
@@ -1,9 +1,42 @@
-function MainViewController() {
+MainViewController.$inject = ['CpM', 'Effectiveness']
+function MainViewController(CpM, Effectiveness) {
   const ctrl = this
-  ctrl.onSelectedAttacker = (p) => console.log(p)
-  ctrl.onSelectedMove = (m) => console.log(m)
-  ctrl.onSelectedDefender = (p) => console.log(p)
-  ctrl.baseAttack = 1
+  // base-attack="$ctrl.baseAttack"
+  // power="$ctrl.power"
+  // base-defense="$ctrl.baseDefense"
+  // defense-cpm="$ctrl.defenseCpm"
+  // stab="$ctrl.stab"
+  // effectiveness="$ctrl.effectiveness"
+  let attacker = null
+  let defender = null
+  let move = null
+  ctrl.onSelectedAttacker = (p) => {
+    attacker = p
+    ctrl.baseAttack = p.stats.baseAttack
+  }
+  ctrl.onSelectedMove = (m) => {
+    move = m
+    ctrl.power = m.power
+    ctrl.stab = m.type === attacker.type || m.type === attacker.type2 ? 1.2 : 1
+    ctrl.effectiveness = computeEffectiveness(m, defender, Effectiveness)
+  }
+  ctrl.onSelectedDefender = (p) => {
+    defender = p
+    ctrl.baseDefense = p.stats.baseDefense
+    ctrl.defenseCpm = p.raidTier ? CpM.bossCpMultiplier[p.raidTier - 1] : CpM.cpMultiplier[38]
+    ctrl.effectiveness = computeEffectiveness(move, defender, Effectiveness)
+  }
+}
+
+function computeEffectiveness(move, defender, effectiveness) {
+  let e = 1
+  if (defender) {
+    e = effectiveness[move.type][defender.type]
+    if (defender.type2) {
+      e *= effectiveness[move.type][defender.type2]
+    }
+  }
+  return e
 }
 
 export default {
