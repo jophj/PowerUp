153f2549a05cdca202d69fe5e6c31eb973906d70
JoP <jop.mezzana@gmail.com>
2017-08-16 16:09:44 +0200

    Pokemon data extracted from gamemaster

---
+++ src/gamemaster/extract-pokemons.js
@@ -0,0 +1,22 @@
+const gamemaster = require('./0691-GAME_MASTER.json')
+
+const pokemonTemplates = gamemaster.itemTemplates.filter(i => i.pokemonSettings)
+const pokemonData = {}
+let ndex = 0
+for (let pokemonTemplate of pokemonTemplates) {
+  ndex += 1
+  const pokemonSettings = pokemonTemplate.pokemonSettings
+  const pokemon = {
+    id: pokemonSettings.pokemonId,
+    ndex: ndex,
+    name: pokemonSettings.pokemonId.charAt(0) + pokemonSettings.pokemonId.slice(1).toLowerCase(),
+    stats: pokemonSettings.stats,
+    type: pokemonSettings.type,
+    type2: pokemonSettings.type2
+  }
+  pokemon.name.replace('_', '-') // Ho-oh
+  pokemon.name.replace('-female', '♀')
+  pokemon.name.replace('-male', '	♂')
+
+  pokemonData[pokemon.id] = pokemon
+}
